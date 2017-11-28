d51b07534d6debe309659bd1b0e4074ce51d4a01
jiangxy7 <jiangxy7@lenovo.com>
2017-11-29 00:51:44 +0800

    添加注释

--- src/com/restful/webservice/server/MainServer.java
+++ src/com/restful/webservice/server/MainServer.java
@@ -5,7 +5,11 @@ import org.apache.cxf.endpoint.Server;
 import org.apache.cxf.jaxrs.JAXRSServerFactoryBean;
 
 public class MainServer {
-
+    /**
+     * 项目为java工程,须下载Apache CXF 框架jar包并加入到项目的lib目录下
+     * 项目才可运行
+     * @param args
+     */
     public static void main(String[] args) {
         JAXRSServerFactoryBean serverFactoryBean = new JAXRSServerFactoryBean();
         serverFactoryBean.setServiceClass(StudentService.class);
